22215470df535f886a2acace70fd62c6a71af2e9
yocajii <yocajii@stone.sakura.ne.jp>
2022-02-08 14:22:22 +0900

    日本語のアイテムがあった場合に整列が崩れていたため修正した

--- 05.ls/ls2.rb
+++ 05.ls/ls2.rb
@@ -32,8 +32,8 @@ class Ls
     rows = (items.size.to_f / COLS).ceil
     horizontal_table = items.each_slice(rows).to_a
     aligned_horizontal_table = horizontal_table.map do |col_data|
-      width = col_data.map(&:size).max + SPAN
-      col_data.map { |item| item.ljust(width) }
+      width = col_data.map { |item| digit_size(item) }.max + SPAN
+      col_data.map { |item| digit_ljust(width, item) }
     end
     aligned_horizontal_table.map { |item| item.values_at(0...rows) }.transpose
   end
@@ -43,6 +43,16 @@ class Ls
       row_data.compact.each { |item| print item }
       print "\n"
     end
+  end
+
+  # 以下は全角文字に対応するためのメソッド
+  def digit_size(text)
+    text.each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.inject(:+)
+  end
+
+  def digit_ljust(width, text)
+    padding = ' ' * (width - digit_size(text))
+    text + padding
   end
 end
 
