e5592dc9685ec610d3c0b6f21b3fde5f4d7984fd
yocajii <yocajii@stone.sakura.ne.jp>
2022-05-09 17:02:33 +0900

    require_relative "frame"のタイミングを修正

--- 08.ls_object/bowling.rb
+++ 08.ls_object/bowling.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'game'
-require_relative 'frame'
 
 game = Game.new(ARGV[0])
 puts game.point
--- 08.ls_object/game.rb
+++ 08.ls_object/game.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'frame'
+
 class Game
   def initialize(score)
     @score = score
