18cd7af1e336ee302d618e7da10fd8747c636c75
yocajii <yocajii@stone.sakura.ne.jp>
2022-06-02 18:37:04 +0900

    最終更新日時に関わるテストを修正
    - 半年以内かどうかでの表示テストを追加した
    - Item.mtimeをDate型のまま比較するようにした
    - sampleディレクトリ内のファイル更新に伴って期待値が変わるテストを修正した

--- spec/08.ls_object/ls_spec.rb
+++ spec/08.ls_object/ls_spec.rb
@@ -9,38 +9,43 @@ RSpec.describe Ls do
     example 'オプションなしの時はshort形式で返す' do
       ls = Ls.new(File.expand_path('../sample', __FILE__), { a: false, l: false, r: false })
       expect(ls.show).to eq <<~TEXT.chomp
-        123      sample.txt  サンプル
-        123.txt  sbit        サンプル.txt
-        sample   slink
+        123      sample      サンプル
+        123.txt  sample.txt  サンプル.txt
+        new      sbit
+        old      slink
       TEXT
     end
 
     example 'aオプションありの時は隠しファイルも返す' do
       ls = Ls.new(File.expand_path('../sample', __FILE__), { a: true, l: false, r: false })
       expect(ls.show).to eq <<~TEXT.chomp
-        .        .hidden      sbit
-        ..       .hidden.txt  slink
-        123      sample       サンプル
-        123.txt  sample.txt   サンプル.txt
+        .        .hidden.txt  sbit
+        ..       new          slink
+        123      old          サンプル
+        123.txt  sample       サンプル.txt
+        .hidden  sample.txt
       TEXT
     end
 
     example 'rオプションありの時はファイル名の降順でソートする' do
       ls = Ls.new(File.expand_path('../sample', __FILE__), { a: false, l: false, r: true })
       expect(ls.show).to eq <<~TEXT.chomp
-        サンプル.txt  sbit        123.txt
-        サンプル      sample.txt  123
-        slink         sample
+        サンプル.txt  sample.txt  123.txt
+        サンプル      sample      123
+        slink         old
+        sbit          new
       TEXT
     end
 
     example 'lオプションありの時はlong形式で返す' do
       ls = Ls.new(File.expand_path('../sample', __FILE__), { a: false, l: true, r: false })
       expect(ls.show).to eq <<~TEXT.chomp
-        total 12
-        drwxr-xr-x 2 yocajii yocajii 4096 May 22 14:48 123
+        total 20
+        drwxr-xr-x 2 yocajii yocajii 4096 Jun  2 12:00 123
         -rw-r--r-- 1 yocajii yocajii    0 May 22 09:37 123.txt
-        drwxr-xr-x 2 yocajii yocajii 4096 May 22 09:57 sample
+        drwxr-xr-x 2 yocajii yocajii 4096 Jun  2 18:14 new
+        drwxr-xr-x 2 yocajii yocajii 4096 Jun  2 18:06 old
+        drwxr-xr-x 2 yocajii yocajii 4096 Jun  2 18:12 sample
         -rw-r--r-- 1 yocajii yocajii    0 May 22 09:37 sample.txt
         -rwxrwxrwt 1 yocajii yocajii    0 May 22 10:03 sbit
         lrwxrwxrwx 1 yocajii yocajii    7 May 22 10:02 slink -> 123.txt
@@ -57,6 +62,24 @@ RSpec.describe Ls do
     example 'lオプションありでアイテム数0の時はtotalのみ返す' do
       ls = Ls.new(File.expand_path('../sample/123', __FILE__), { a: false, l: true, r: false })
       expect(ls.show).to eq 'total 0'
+    end
+
+    context 'lオプションの最終更新日時' do
+      example '半年以上前の場合は月日年の書式で返す' do
+        ls = Ls.new(File.expand_path('../sample/old', __FILE__), { a: false, l: true, r: false })
+        expect(ls.show).to eq <<~TEXT.chomp
+          total 0
+          -rw-r--r-- 1 yocajii yocajii 0 Nov 11  2021 old.txt
+        TEXT
+      end
+
+      example '半年以内の場合は月日時刻の書式で返す' do
+        ls = Ls.new(File.expand_path('../sample/new', __FILE__), { a: false, l: true, r: false })
+        expect(ls.show).to eq <<~TEXT.chomp
+          total 0
+          -rw-r--r-- 1 yocajii yocajii 0 Jun  2 18:14 new.txt
+        TEXT
+      end
     end
   end
 end
